af7ed15b8ffe1718c37c9eb936a7210fd3091f43
SimonBalu <91798996+SimonBalu@users.noreply.github.com>
2022-10-24 21:41:33 +0200

    1.7

---
+++ dorfkind-test/traktoren.js
@@ -0,0 +1,13 @@
+function traktoren() {
+
+
+  //Variablen:
+    var eingabe1 = document.getElementById('hersteller1').value;
+
+//eingabe1
+    if (eingabe1 == "Fendt") {
+      alert("1");
+    }
+
+
+};
